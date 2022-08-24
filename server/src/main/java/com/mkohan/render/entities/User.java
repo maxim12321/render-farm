f311740e05e886bc2a290bd1f21aa7d50bc65ff7
Max <mkogan58@gmail.com>
2022-08-24 18:42:32 +0300

    Add User table and entity

---
+++ server/src/main/java/com/mkohan/render/entities/User.java
@@ -0,0 +1,29 @@
+package com.mkohan.render.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
