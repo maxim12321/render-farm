95269e44a20f10a333bd5a16b495d9a5343fadf9
Max <mkogan58@gmail.com>
2022-08-26 18:46:09 +0300

    Provide more readable messages

--- client/src/main/java/com/mkohan/render/RenderFarmClientApplication.java
+++ client/src/main/java/com/mkohan/render/RenderFarmClientApplication.java
@@ -1,5 +1,9 @@
 package com.mkohan.render;
 
+import com.mkohan.render.dtos.TaskDto;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Scanner;
 
 public class RenderFarmClientApplication {
@@ -82,14 +86,52 @@ public class RenderFarmClientApplication {
     }
 
     private static void tasks(RenderFarmApi api) {
-        api.getAllTasks().forEach(System.out::println);
+        final String separator = "-----------------------------------";
+        final List<TaskDto> tasks = api.getAllTasks();
+
+        if (!tasks.isEmpty()) {
+            System.out.println(separator);
+            System.out.println("|    ID    |        STATUS        |");
+            System.out.println(separator);
+
+            tasks.forEach(task -> System.out.printf("|   %-7s|       %-15s|\n", task.getId(), task.getStatus()));
+
+            System.out.println(separator);
+        }
     }
 
     private static void task(RenderFarmApi api, Scanner scanner) {
+        final SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+        final SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
+
+        final String separator = "-------------------------------------------------";
+        final String row = "| %s at %s |       %s      |\n";
+
         System.out.print("Task ID: ");
         final long id = scanner.nextLong();
         scanner.nextLine();
-        api.getTaskById(id).ifPresent(System.out::println);
+
+        api.getTaskById(id).ifPresent(task -> {
+            System.out.println(separator);
+            System.out.println("|          DATE          |        STATUS        |");
+            System.out.println(separator);
+
+            System.out.printf(row,
+                    date.format(task.getSubmittedAt()),
+                    time.format(task.getSubmittedAt()),
+                    "SUBMITTED"
+            );
+
+            if (task.getCompletedAt() != null) {
+                System.out.printf(row,
+                        date.format(task.getCompletedAt()),
+                        time.format(task.getCompletedAt()),
+                        "COMPLETED"
+                );
+            }
+
+            System.out.println(separator);
+        });
     }
 
     private static void submit(RenderFarmApi api) {
